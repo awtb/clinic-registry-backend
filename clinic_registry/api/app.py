64df9179eb880ea7be1d06843a6645c994dd2ef2
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-02-12 18:25:11 +0500

    feat(api): Include medical-records router.

--- clinic_registry/api/app.py
+++ clinic_registry/api/app.py
@@ -7,6 +7,7 @@ from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from clinic_registry.api.routers.auth import router as auth_router
+from clinic_registry.api.routers.medical_record import router as records_router
 from clinic_registry.api.routers.patient import router as patients_router
 from clinic_registry.api.routers.user import router as users_router
 from clinic_registry.settings import Settings
@@ -67,5 +68,6 @@ def build_app() -> FastAPI:
     app.include_router(auth_router)
     app.include_router(users_router)
     app.include_router(patients_router)
+    app.include_router(records_router)
 
     return app
