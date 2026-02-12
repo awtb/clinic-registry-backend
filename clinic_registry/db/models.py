9f50b278b3c218083205857e8353fac471ab48da
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-02-12 18:18:02 +0500

    feat(medical-records): Add medical records table.

--- clinic_registry/db/models.py
+++ clinic_registry/db/models.py
@@ -4,12 +4,15 @@ from datetime import datetime
 from sqlalchemy import Boolean
 from sqlalchemy import Date
 from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 from ulid import ULID
 
+from clinic_registry.core.dto.medical_record import MedicalRecordDTO
 from clinic_registry.core.dto.patient import PatientDTO
 from clinic_registry.core.dto.user import UserDTO
 from clinic_registry.core.enums.patient import PatientGender
@@ -106,4 +109,53 @@ class Patient(BaseModel):
             created_at=self.created_at,
             updated_at=self.updated_at,
             last_visit=self.last_visit,
+        )
+
+
+class MedicalRecord(BaseModel):
+    __tablename__ = "medical_records"
+
+    id: Mapped[str] = mapped_column(
+        String(),
+        nullable=False,
+        primary_key=True,
+        default=lambda: str(ULID()),
+    )
+    patient_id: Mapped[str] = mapped_column(
+        String(),
+        ForeignKey("patients.id"),
+        nullable=False,
+    )
+    diagnosis: Mapped[str] = mapped_column(String(), nullable=False)
+    treatment: Mapped[str] = mapped_column(String(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(), nullable=False, default=datetime.now
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(), nullable=False, default=datetime.now
+    )
+    chief_complaint: Mapped[str] = mapped_column(String(), nullable=True)
+    creator_id: Mapped[str] = mapped_column(
+        String(),
+        ForeignKey("users.id"),
+        nullable=False,
+    )
+    creator: Mapped[User] = relationship("User", foreign_keys=[creator_id])
+    patient: Mapped[Patient] = relationship(
+        "Patient",
+        foreign_keys=[patient_id],
+    )
+    procedures: Mapped[str] = mapped_column(String(), nullable=False)
+
+    def to_dto(self) -> MedicalRecordDTO:
+        return MedicalRecordDTO(
+            id=self.id,
+            patient_id=self.patient_id,
+            diagnosis=self.diagnosis,
+            treatment=self.treatment,
+            created_at=self.created_at,
+            updated_at=self.updated_at,
+            chief_complaint=self.chief_complaint,
+            creator_id=self.creator_id,
+            procedures=self.procedures,
         )
