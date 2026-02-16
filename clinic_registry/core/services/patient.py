e79e17add36c6e0392ef3d563860b086770f9fec
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-02-16 19:08:01 +0500

    feat(patients): Implement update-patient.

--- clinic_registry/core/services/patient.py
+++ clinic_registry/core/services/patient.py
@@ -1,6 +1,10 @@
 from clinic_registry.core.dto.base import PageDTO
 from clinic_registry.core.dto.patient import PatientCreateDTO
 from clinic_registry.core.dto.patient import PatientDTO
+from clinic_registry.core.dto.patient import PatientUpdateDTO
+from clinic_registry.core.dto.user import CurrentUserDTO
+from clinic_registry.core.enums.user import UserRole
+from clinic_registry.core.errors.common import NotAllowedError
 from clinic_registry.core.errors.common import NotFoundError
 from clinic_registry.core.repos.patient import PatientRepository
 
@@ -16,6 +20,29 @@ class PatientService:
 
         if patient is None:
             raise NotFoundError("Patient not found")
+
+        return patient
+
+    async def update_patient(
+        self, current_user: CurrentUserDTO, request: PatientUpdateDTO
+    ) -> PatientDTO:
+        # TODO(Ilyas): we should have separate method for RBAC checks.
+        if current_user.role != UserRole.admin:
+            raise NotAllowedError("Only admins can create users")
+
+        await self._patient_repo.update_patient(
+            request.patient_for_update,
+            first_name=request.first_name,
+            last_name=request.last_name,
+            date_of_birth=request.date_of_birth,
+            passport=request.passport_number,
+            notes=request.notes,
+            phone=request.phone_number,
+        )
+
+        patient = await self.get_patient(
+            request.patient_for_update.id,
+        )
 
         return patient
 
