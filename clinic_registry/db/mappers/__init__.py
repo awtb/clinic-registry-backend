a4729fd2c62b3f4751dee18758fbbdcbbfb8d038
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-04-25 15:03:36 +0500

    chore(db): export procedure mappers

--- clinic_registry/db/mappers/__init__.py
+++ clinic_registry/db/mappers/__init__.py
@@ -1,11 +1,17 @@
 from clinic_registry.db.mappers.log import log_to_dto
 from clinic_registry.db.mappers.medical_record import medical_record_to_dto
 from clinic_registry.db.mappers.patient import patient_to_dto
+from clinic_registry.db.mappers.procedure import procedure_to_dto
+from clinic_registry.db.mappers.procedure_category import (
+    procedure_category_to_dto,
+)
 from clinic_registry.db.mappers.user import user_to_dto
 
 __all__ = [
     "user_to_dto",
     "patient_to_dto",
+    "procedure_category_to_dto",
+    "procedure_to_dto",
     "medical_record_to_dto",
     "log_to_dto",
 ]
