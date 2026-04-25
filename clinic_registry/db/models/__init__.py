75eaa8aba41d0724f5c0d3a535a1dac2a187ca42
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-04-25 15:02:39 +0500

    chore(db): export procedure models

--- clinic_registry/db/models/__init__.py
+++ clinic_registry/db/models/__init__.py
@@ -1,7 +1,21 @@
 from clinic_registry.db.models.base import BaseModel
 from clinic_registry.db.models.log import Log
 from clinic_registry.db.models.medical_record import MedicalRecord
+from clinic_registry.db.models.medical_record_procedure import (
+    medical_record_procedures,
+)
 from clinic_registry.db.models.patient import Patient
+from clinic_registry.db.models.procedure import Procedure
+from clinic_registry.db.models.procedure_category import ProcedureCategory
 from clinic_registry.db.models.user import User
 
-__all__ = ["BaseModel", "User", "Patient", "MedicalRecord", "Log"]
+__all__ = [
+    "BaseModel",
+    "User",
+    "Patient",
+    "ProcedureCategory",
+    "Procedure",
+    "MedicalRecord",
+    "medical_record_procedures",
+    "Log",
+]
